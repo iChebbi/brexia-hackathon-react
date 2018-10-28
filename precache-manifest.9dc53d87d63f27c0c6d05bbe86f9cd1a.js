43726ecabac8e5afb6e7be586c5122d28b74f33c
Chebbi Aymen <aymen.chebi@gmail.com>
2018-10-28 08:56:40 +0100

    Updates

---
+++ precache-manifest.9dc53d87d63f27c0c6d05bbe86f9cd1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11d3034399cb30380913",
+    "url": "/brexia-hackathon-react/static/js/runtime~main.11d30343.js"
+  },
+  {
+    "revision": "3c1354a998cd2b4e0aa6",
+    "url": "/brexia-hackathon-react/static/js/main.3c1354a9.chunk.js"
+  },
+  {
+    "revision": "eefb07b39a61b8d911c1",
+    "url": "/brexia-hackathon-react/static/js/1.eefb07b3.chunk.js"
+  },
+  {
+    "revision": "3c1354a998cd2b4e0aa6",
+    "url": "/brexia-hackathon-react/static/css/main.57a70374.chunk.css"
+  },
+  {
+    "revision": "eefb07b39a61b8d911c1",
+    "url": "/brexia-hackathon-react/static/css/1.d9fc84e6.chunk.css"
+  },
+  {
+    "revision": "78582b3929476781fe7abc845aeece88",
+    "url": "/brexia-hackathon-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/brexia-hackathon-react/precache-manifest.9dc53d87d63f27c0c6d05bbe86f9cd1a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/brexia-hackathon-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
